137f23c6cfd31e21f0de5e770d2b18ff21c1b026
Venkata Sri Sai Renu <renu.venkata@proclink.local>
2022-09-14 09:04:20 +0530

    new folder added

---
+++ array.js
@@ -0,0 +1,7 @@
+var arr=[43,355,989,223,323,325,399];
+console.log(arr);
+console.log(arr.length);
+console.log(arr[arr.length]);
+console.log(arr[arr.length-1]);
+console.log(arr[arr.length-2]);
+arr=new Array()
---
+++ index.js
@@ -0,0 +1,13 @@
+// console.log("Hi");
+// var name="Renu";
+// console.log(name);
+// console.log("name");
+// console.log(name+3);
+// var i=5;
+// console.log(i==="5");
+var  marks=[70,80,90,98,79]
+//marks=3;
+console.log(marks[0]);
+console.log(marks.length)
+console.log(marks.reverse());
+console.log(marks[marks.length-1]);
